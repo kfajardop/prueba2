31f37b666cb8cc37a6d6f9dddecbd5f1d188c0d3
keisyf <149024294+keisyf@users.noreply.github.com>
2023-10-25 18:02:25 -0600

    cambio1 usuario

--- prueba2.py
+++ prueba2.py
@@ -5,3 +5,17 @@ print(suma)
 
 resta=a-b
 print(resta)
+
+import tkinter as tk
+
+# Crear una ventana de Tkinter
+ventana = tk.Tk()
+ventana.title("Cambio de color de fondo")
+
+# Configurar el color de fondo de la ventana
+ventana.configure(bg="blue")  # Puedes cambiar "blue" al color que desees
+
+# Añadir elementos a la ventana (por ejemplo, etiquetas, botones, etc.)
+
+# Mostrar la ventana
+ventana.mainloop()
